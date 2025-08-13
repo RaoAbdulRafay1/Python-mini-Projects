17278e1dbc8f1222775a7bc364a06d2d3f04a02e
RaoAbdulRafay1 <raoabdulrafay24@gmail.com>
2025-08-13 19:27:40 +0500

    Completed todo class

--- Todo_List.py
+++ Todo_List.py
@@ -6,9 +6,34 @@ class Todo_List:
         with open(self.file_name,"a") as f:
             f.write(task + "\n")
             self.tasks.append(task)
+    def view_task(self):
+        try:
+            with open(self.file_name,"r") as f:
+                tasks = f.readlines()
+                if not tasks:
+                    print("No tasks found.")
+                else:
+                    print("Tasks:")
+                    for i, task in enumerate(tasks, 1):
+                        print(f"{i}. {task.strip()}")
+        except FileNotFoundError:
+            print("No tasks found. Please add a task first.")
+    def delete_task(self,task):
+        with open(self.file_name,"r") as f:
+            self.tasks =f.readlines()
+            if task + "\n" in self.tasks:
+                self.tasks.remove(task + "\n")
+            else:
+                print("Task not found")
+        with open(self.file_name,"w") as f:
+            f.writelines(self.tasks)
+        print(f"Successfully deleted {task}\n")
+                        
+
 
 todo =Todo_List()
-todo.add_task("Home")
-todo.add_task("Homeworkk")
-todo.add_task("Homehahah")
+todo.view_task()
+todo.delete_task("Homehahah")
+todo.view_task()
+
 
