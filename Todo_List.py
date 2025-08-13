5389ca618d682596def501662995b8df8d7ce253
RaoAbdulRafay1 <raoabdulrafay24@gmail.com>
2025-08-13 20:12:53 +0500

    Added menu for app

--- Todo_List.py
+++ Todo_List.py
@@ -6,6 +6,7 @@ class Todo_List:
         with open(self.file_name,"a") as f:
             f.write(task + "\n")
             self.tasks.append(task)
+        print(f"Successfully added {task}.")    
     def view_task(self):
         try:
             with open(self.file_name,"r") as f:
@@ -27,13 +28,30 @@ class Todo_List:
                 print("Task not found")
         with open(self.file_name,"w") as f:
             f.writelines(self.tasks)
-        print(f"Successfully deleted {task}\n")
-                        
+        print(f"Successfully deleted {task}")
 
-
-todo =Todo_List()
-todo.view_task()
-todo.delete_task("Homehahah")
-todo.view_task()
-
+print("========= Welcome to Todo app ==========")
+print("1--Add Task")
+print("2--View Task")
+print("3--Delete Task")
+print("4--Exit ")
+todo = Todo_List()
+check = 1
+while  check:
+    choice = input("Enter your choice :")
+    if choice == "1":
+        task = input("Enter Task :")
+        todo.add_task(task)
+    elif choice =="2":
+        todo.view_task()
+    elif choice =="3":
+        task =input("Enter Task you want to delete :")
+        todo.delete_task(task)
+    elif choice =="4":
+        print("Exiting App.....")
+        check = 0
+    else:
+        print("Invalid Option. Please choose valid option")
+        
+        
 
