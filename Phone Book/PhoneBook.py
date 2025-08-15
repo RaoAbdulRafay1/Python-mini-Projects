1a72628fe8ff1345eccf16e82a50e4d455648fb4
RaoAbdulRafay1 <raoabdulrafay24@gmail.com>
2025-08-15 20:23:21 +0500

    Added update feature

--- Phone Book/PhoneBook.py
+++ Phone Book/PhoneBook.py
@@ -15,16 +15,23 @@ class PhoneBook :
         for contact in self.contacts:
             if name in contact:
                 self.contacts.remove(contact)
-                print(f"{contact} was deleted")            
+                print(f"{contact} was deleted")
+    def update_contact(self,cont,name):
+        for i, contact in enumerate(self.contacts):
+            if contact["name"] == name:
+                self.contacts[i] = cont
+                print(f"{cont} was updated")
 pb= PhoneBook()
-pb.add_contact({"Rao" : "033333333"})
-pb.add_contact({"Rao1" : "032333333"})
-pb.add_contact({"Rao2" : "035333333"})                     
+pb.add_contact({"name": "Rao" , "phone": "033333333"})
+pb.add_contact({"name": "Rao1" , "phone": "032333333"})
+pb.add_contact({"name": "Rao2" , "phone": "035333333"})
 pb.display_contacts()
 print("Search ")
 pb.search_contact("Rao1")
 
 pb.delete_contact("Rao")
 pb.display_contacts()
-
+pb.update_contact({"name":"Rao","phone":"23111231"},"Rao")
+print("After update")
+pb.display_contacts()
 
