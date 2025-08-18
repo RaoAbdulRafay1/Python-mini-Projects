9dd33d836128f00859f50874d5e4c44018c2b817
RaoAbdulRafay1 <raoabdulrafay24@gmail.com>
2025-08-18 18:20:33 +0500

    Added Contact class

--- Phone Book/PhoneBook.py
+++ Phone Book/PhoneBook.py
@@ -1,37 +1,43 @@
+class Contact:
+    def __init__(self):
+        self.info = {}
+    def set_contact(self,dic):
+        self.info = dic
+    def get_contact(self):
+        return self.info
+    
 class PhoneBook :
     def __init__(self):
        self.contacts = []
     def add_contact(self,contact):
         self.contacts.append(contact)
-        print(f"Contact {contact} was added")
+        print(f"Contact {contact.get_contact()} was added")
     def display_contacts(self):
         for i in self.contacts :
-            print(i)
+            print(i.get_contact())
     def search_contact(self,name):
         for contact in self.contacts:
             if name in contact:
-                print(f"Found contact  {contact} .")
+                print(f"Found contact  {contact.get_contact()} .")
     def delete_contact(self,name):
         for contact in self.contacts:
             if name in contact:
                 self.contacts.remove(contact)
-                print(f"{contact} was deleted")
+                print(f"{contact.get_contact()} was deleted")
     def update_contact(self,cont,name):
-        for i, contact in enumerate(self.contacts):
-            if contact["name"] == name:
-                self.contacts[i] = cont
-                print(f"{cont} was updated")
+        for contact in self.contacts:
+            if name in contact:
+                self.contacts.remove(contact)
+                self.contacts.append(cont)
+                print(f"{contact.get_contact()} was updated to {cont.get_contact()}")
 pb= PhoneBook()
-pb.add_contact({"name": "Rao" , "phone": "033333333"})
-pb.add_contact({"name": "Rao1" , "phone": "032333333"})
-pb.add_contact({"name": "Rao2" , "phone": "035333333"})
-pb.display_contacts()
-print("Search ")
-pb.search_contact("Rao1")
+print("Enter name,phone and email")
+name =input("Name :")
+phone = input("Phone :")
+email = input("Email:")
+c =Contact()
+c.set_contact({"Name":name,"Phone":phone,"Email":email})
 
-pb.delete_contact("Rao")
-pb.display_contacts()
-pb.update_contact({"name":"Rao","phone":"23111231"},"Rao")
-print("After update")
-pb.display_contacts()
 
+pb.add_contact(c)
+pb.display_contacts()
